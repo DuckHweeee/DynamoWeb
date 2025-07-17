67f30d08df50f8f95cbd9dceec48406011fc0dd8
Bùi Hoàng Phúc <phuc362001@gmail.com>
2025-07-17 14:01:26 +0700

    #3-dynamo-web-ui

--- demo_dynamo_website/app/page.tsx
+++ demo_dynamo_website/app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-2 px-4 py-3 bg-white rounded-[10px] shadow">
-      Hello World!!!!
     </div>
   );
 }
