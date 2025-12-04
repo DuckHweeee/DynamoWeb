1b66098b59daed978c45d5b035006ae05feeaae6
DuckHweeee <huy.mai.set18@eiu.edu.vn>
2025-12-04 09:09:11 +0700

    04/12/2025 commit

--- demo_dynamo_website/components/sidebar/logo.tsx
+++ demo_dynamo_website/components/sidebar/logo.tsx
@@ -2,18 +2,19 @@
 import { useSidebar } from "@/components/ui/sidebar"
 import { House } from "lucide-react"
 import * as React from "react"
+import Link from "next/link"
 
 
 export function Logo() {
     const { open } = useSidebar()
     return (
         <>
-            <div>
-                {/* {open ?
-                    // <img className="h-auto w-auto" src="/logo.png" alt="Logo" />
+            <Link href="/"   className="flex items-center justify-center pt-3">
+                {open ?
+                    <img className="h-auto w-auto" src="/Layer_2.png" alt="Logo" />
                     : (<House className="ml-1.5 mt-2" />)
-                } */}
-            </div>
+                }
+            </Link>
         </>
 
     )
